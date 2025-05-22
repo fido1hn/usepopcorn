87d0fcfb0a5560b9ff2a5c32ce99682ebfecbd26
fido1hn <confidoekeanya@gmail.com>
2025-05-22 23:48:32 +0100

    useEffect lesson

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-// import './index.css'
-// import App from './App.jsx'
+import "./index.css";
+import App from "./App.jsx";
 
 import StarRating from "./components/StarRating";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <StarRating
-      maxRating={5}
-      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
-    />
-    <StarRating size={24} color="red" className="test" defaultRating={3} />
+    <App />
   </StrictMode>
 );
